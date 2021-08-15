c830954efd0e99469317e07f7d6e0df761ba7749
Koech-code <nixon.koech@moringaschool.com>
2021-08-15 12:35:41 +0300

    added more functionalities and database to my app

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='c8a6093f95cf391988a0e610e1d878d4'
 export SECRET_KEY='7166'
+export MAIL_USERNAME=kipkorirkoech098@gmail.com
+export MAIL_PASSWORD=koech1234
 
 python3.6 manage.py server
